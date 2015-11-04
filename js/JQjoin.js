dd848648bdff4400c27a74459f08aa67875b4dda
Dana <djmedhaug@gmail.com>
2015-11-04 10:06:12 -0800

    updated

--- js/JQjoin.js
+++ js/JQjoin.js
@@ -5,26 +5,26 @@ $(function() {
 });
 
 $('#submit').click(function(event) {
-        var a = 0;
+        var pass = 0;
         event.preventDefault();
 
     if ($('[name="name"]').val().length < 1) {
         window.alert("Type in your name!");
-        a++; 
+        pass++; 
     };
 
     if ($('[name="username"]').val().length < 1) {
         window.alert("Type in your username!");
-        a++;
+        pass++;
     };
 
 
     if (emailCheck.test($('[name="email"]').val()) === false) {
         window.alert('Type in a valid email!!!');
-        a++;
+        pass++;
     };
 
-    if (a === 0) {
+    if (pass === 0) {
         var $name = $('[name="name"]').val();
         window.location.href = "gallery.html?" + $name;
     };
