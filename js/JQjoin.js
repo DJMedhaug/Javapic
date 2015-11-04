396a04792a0d7cc607dc24d08bc17ad950e570ea
Dana <djmedhaug@gmail.com>
2015-11-04 10:03:35 -0800

    updated

--- js/JQjoin.js
+++ js/JQjoin.js
@@ -5,22 +5,28 @@ $(function() {
 });
 
 $('#submit').click(function(event) {
+        var a = 0;
         event.preventDefault();
 
     if ($('[name="name"]').val().length < 1) {
         window.alert("Type in your name!");
+        a++; 
     };
 
     if ($('[name="username"]').val().length < 1) {
         window.alert("Type in your username!");
+        a++;
     };
 
 
     if (emailCheck.test($('[name="email"]').val()) === false) {
         window.alert('Type in a valid email!!!');
-        event.preventDefault();
+        a++;
     };
 
-var $name = $('[name="name"]').val();
-    window.location.href = "gallery.html?" + $name;
+    if (a === 0) {
+        var $name = $('[name="name"]').val();
+        window.location.href = "gallery.html?" + $name;
+    };
 });
+
